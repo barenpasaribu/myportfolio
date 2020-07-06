d646e69e5ecba9be119ecf54b95741ab7ef2853a
barenpasaribu <baren.pasaribu@gmail.com>
2020-07-06 15:29:49 +0700

    modify app.js adding react-mdl library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import Main from './components/Main'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        my portfolio web
-      </header>
-    </div >
+    <div className="demo-big-content">
+      <Layout>
+        <Header className="header-color" title="MY PORTFOLIO" scroll>
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/about">About Me</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="Navbar">
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/about">About Me</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
+    </div>
   );
 }
 
